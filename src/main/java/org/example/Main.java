ab7296a9dc76776e1e721ecc478a432154d8fa7f
Devon Morrison <devmorri421@gmail.com>
2023-06-28 18:33:46 -0400

    add test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        //test
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
